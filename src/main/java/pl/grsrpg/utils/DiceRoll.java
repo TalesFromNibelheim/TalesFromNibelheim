d79f5285394e175286cce43ea5eb07bc71384825
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-05-21 18:31:01 +0200

    Update DiceRoll.java

--- src/main/java/pl/grsrpg/utils/DiceRoll.java
+++ src/main/java/pl/grsrpg/utils/DiceRoll.java
@@ -8,10 +8,10 @@ public class DiceRoll {
     private static final Random random = new Random();
 
     public static int rollDiceMove() {
-        return random.nextInt(Game.getConfig().getDiceMax() + 1) + Game.getConfig().getDiceMin();
+        return random.nextInt(Game.getConfig().getDiceMax()) + Game.getConfig().getDiceMin();
     }
 
-    public static int rollDic(int min, int max) {
-        return random.nextInt(max + 1) + min;
+    public static int rollDice(int min, int max) {
+        return random.nextInt(max) + min;
     }
 }
