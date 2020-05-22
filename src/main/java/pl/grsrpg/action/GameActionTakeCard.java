a868817deec56134927618ae734efd8d287ee195
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-05-22 14:53:41 +0200

    Update GameActionTakeCard.java

--- src/main/java/pl/grsrpg/action/GameActionTakeCard.java
+++ src/main/java/pl/grsrpg/action/GameActionTakeCard.java
@@ -1,11 +1,11 @@
 package pl.grsrpg.action;
 
-import pl.grsrpg.card.GameCard;
+import pl.grsrpg.card.Card;
 import pl.grsrpg.player.Player;
 
 
 public class GameActionTakeCard extends GameAction{
-    private GameCard card;
+    private Card card;
 
     @Override
     public boolean execute(Player player){
