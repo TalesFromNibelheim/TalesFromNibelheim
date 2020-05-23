3ca10e39ac43166e1b1406a4a22c049d12148ae5
konrad.gabrukiewicz@gmail.com <konrad.gabrukiewicz@gmail.com>
2020-05-23 12:44:50 +0200

    FIrst spell to warriorClass

--- src/main/java/pl/grsrpg/player/GamePlayerWarrior.java
+++ src/main/java/pl/grsrpg/player/GamePlayerWarrior.java
@@ -1,9 +1,17 @@
 package pl.grsrpg.player;
 
 import pl.grsrpg.field.Field;
+import pl.grsrpg.utils.DiceRoll;
 
 public class GamePlayerWarrior extends GamePlayer {
     public GamePlayerWarrior(String name, Field currentField) {
         super(name, 20, 15, 5, 0, 20, currentField);
+    }
+
+    int knockdown(){
+        this.magicPoints  -= 5;
+        if(DiceRoll.rollPrivate(1,6) >= 4)
+            System.out.println("Zadajesz dodatkowe obrazenia przy uderzeniu:" + "wartosc do ustalenia");
+        return 0 ;// tu bedzie ile obrazen jak sie wymysli
     }
 }
