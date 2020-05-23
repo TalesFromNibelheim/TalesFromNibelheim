604758627f0aebcbbd38f20fa511c49f4d69836a
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-05-23 13:12:31 +0200

    Add Attribiute enum.

--- src/main/java/pl/grsrpg/action/GameActionCoach.java
+++ src/main/java/pl/grsrpg/action/GameActionCoach.java
@@ -1,22 +1,30 @@
 package pl.grsrpg.action;
 
 import pl.grsrpg.player.Player;
+import pl.grsrpg.utils.Attribute;
 
 public class GameActionCoach extends GameAction {
 
     protected float price;
-    protected float maxHealth;
-    protected float strength;
-    protected float agility;
-    protected float magicPoints;
 
     @Override
     public boolean execute (Player player){
         return true;
     }
 
-    void textMenu(){ // co wyswietla coach
-
+    private int getPrice(Player player, Attribute attribute){
+        switch (attribute){
+            case AGILITY:
+                return (int) (price * Math.log(player.getAgility()));
+            case STRENGTH:
+                return (int) (price * Math.log(player.getStrength()));
+            case MAXHEALTH:
+                return (int) (price * Math.log(player.getMaxHealth()));
+            case MAGICPOINTS:
+                return (int) (price * Math.log(player.getMagicPoints()));
+            default:
+                return 0;
+        }
     }
 
 }
